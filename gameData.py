d8f6fc44eb36aaf7183971bc331c6935328a24fb
adhamfarag63-oss <adhamfarag63@gmail.com>
2026-09-21 12:01:53 -0700

    Refactor GameData class and enhance documentation
    Refactor GameData class to improve structure and clarity. Added detailed docstrings and adjusted player and enemy movement methods.

--- gameData.py
+++ gameData.py
@@ -1,195 +1,199 @@
-""" TODO
-Author: Your name here!
-Add a description here!
+"""
+Name: Adham Farag
+Date: September 20, 2026
+Description: Manages the game data and board state for Antarctic Survival (ENGR 221 Lab 2).
+Handles player movement, neighbor retrieval, food generation, and enemy mechanics.
 """
 
 import random
-
-from cell import Cell
 from preferences import Preferences
+from cell import Cell
 
 class GameData:
     def __init__(self):
-        # The current state of the board
-        self.board = [[Cell(row, col) for col in range(Preferences.NUM_COLS)] 
-                                      for row in range(Preferences.NUM_ROWS)]
+        """Initializes the board state, score, steps, and entity positions."""
+        self.num_rows = Preferences.NUM_ROWS
+        self.num_cols = Preferences.NUM_COLS
         
-        # Whether or not the game is over
-        self.gameover = False
-
-        # The current cell containing the player
-        self.player = self.board[0][0]    # Start at the top left
-        self.player.become_player()
-
-        # The number of empty cells on the board, accounting for the player cell
-        self.num_empty_cells = Preferences.NUM_CELLS - 1
-
-        # A list of cells containing food
-        self.food = []
-        # Number of food eaten
-        self.score = 0
-
-        # A list of cells containing enemies
+        # Construct 2D grid of Cell objects
+        self.board = [[Cell(r, c) for c in range(self.num_cols)] for r in range(self.num_rows)]
+        
+        self.player_cell = self.board[0][0]
+        self.player_cell.cell_type = Preferences.CELL_TYPE_PLAYER
+        
+        self.food_cells = []
         self.enemies = []
-
-
-    #######################
-    # Game Limits Methods #
-    #######################
-
-    def at_max_food(self) -> bool:
-        """ Check whether we can add more food """
-        return len(self.food) / self.num_empty_cells > Preferences.MAX_FOOD
-    
-    def at_max_enemies(self) -> bool:
-        """ Check whether we can add more enemies """
-        return len(self.enemies) / self.num_empty_cells > Preferences.MAX_ENEMIES
-
-    def set_game_over(self) -> None:
-        """ Turn on the game over flag """
-        self.gameover = True
-
-
-    ##############################
-    # Neighbor Retrieval Methods #
-    ##############################
-
-    def get_west_neighbor(self, cell: Cell) -> Cell:
-        """ Returns the cell immediately to the left of the given cell. 
-            If we are at the  edge of the map, return None. """
-        # TODO
-        pass # Remove this after you implement your function
         
-    def get_east_neighbor(self, cell: Cell) -> Cell:
-        """ Returns the cell immediately to the right of the given cell.
-            If we are at the edge of the map, return None. """
-        # TODO
-        pass # Remove this after you implement your function
-    
-    def get_north_neighbor(self, cell: Cell) -> Cell:
-        """ Returns the cell immediately above the given cell.
-            If we are at the edge of the map, return None. """
-        # TODO
-        pass # Remove this after you implement your function
+        self.score = 0
+        self.steps = 0
+        self.game_over = False
+
+    # ==========================================
+    # 2.1 Helper Functions: Neighbor Retrieval
+    # ==========================================
+
+    def get_west_neighbor(self, cell):
+        """Returns the Cell directly to the left of given cell, or None if out of bounds."""
+        if cell.col > 0:
+            return self.board[cell.row][cell.col - 1]
+        return None
+
+    def get_east_neighbor(self, cell):
+        """Returns the Cell directly to the right of given cell, or None if out of bounds."""
+        if cell.col < self.num_cols - 1:
+            return self.board[cell.row][cell.col + 1]
+        return None
+
+    def get_north_neighbor(self, cell):
+        """Returns the Cell directly above given cell, or None if out of bounds."""
+        if cell.row > 0:
+            return self.board[cell.row - 1][cell.col]
+        return None
+
+    def get_south_neighbor(self, cell):
+        """Returns the Cell directly below given cell, or None if out of bounds."""
+        if cell.row < self.num_rows - 1:
+            return self.board[cell.row + 1][cell.col]
+        return None
+
+    # ==========================================
+    # 2.2 Player Movement Methods
+    # ==========================================
+
+    def move_player_to_cell(self, target_cell):
+        """Helper to update board state when player moves to a target cell."""
+        if target_cell is None or self.game_over:
+            return
+
+        # If stepping on an enemy, game over
+        if target_cell in self.enemies or target_cell.cell_type == Preferences.CELL_TYPE_ENEMY:
+            self.game_over = True
+            return
+
+        # If stepping on food, eat it
+        if target_cell in self.food_cells or target_cell.cell_type == Preferences.CELL_TYPE_FOOD:
+            self.eat_food(target_cell)
+
+        # Clear old player position
+        self.player_cell.cell_type = Preferences.CELL_TYPE_EMPTY
         
-    def get_south_neighbor(self, cell: Cell) -> Cell:
-        """ Returns the cell immediately below the given cell.
-            If we are at the edge of the map, return None. """
-        # TODO
-        pass # Remove this after you implement your function
+        # Set new player position
+        self.player_cell = target_cell
+        self.player_cell.cell_type = Preferences.CELL_TYPE_PLAYER
 
+        # Increment step count and handle periodic spawns
+        self.steps += 1
+        if self.steps % 10 == 0:
+            self.add_food()
+        if self.steps % 50 == 0:
+            self.add_enemy()
 
-    ###########################
-    # Player Movement Methods #
-    ###########################
+    def move_player_left(self):
+        """Moves the player left one cell if possible."""
+        target = self.get_west_neighbor(self.player_cell)
+        self.move_player_to_cell(target)
+
+    def move_player_right(self):
+        """Moves the player right one cell if possible."""
+        target = self.get_east_neighbor(self.player_cell)
+        self.move_player_to_cell(target)
+
+    def move_player_up(self):
+        """Moves the player up one cell if possible."""
+        target = self.get_north_neighbor(self.player_cell)
+        self.move_player_to_cell(target)
+
+    def move_player_down(self):
+        """Moves the player down one cell if possible."""
+        target = self.get_south_neighbor(self.player_cell)
+        self.move_player_to_cell(target)
+
+    # ==========================================
+    # 2.3 Food Related Methods
+    # ==========================================
+
+    def add_food(self):
+        """Adds a food cell at a random empty location on the board."""
+        empty_cells = [
+            cell for row in self.board for cell in row
+            if cell.cell_type == Preferences.CELL_TYPE_EMPTY
+        ]
+        if empty_cells:
+            target_cell = random.choice(empty_cells)
+            target_cell.cell_type = Preferences.CELL_TYPE_FOOD
+            self.food_cells.append(target_cell)
+
+    def eat_food(self, target_cell):
+        """Updates score and removes food cell when player eats food."""
+        if target_cell in self.food_cells:
+            self.food_cells.remove(target_cell)
+        target_cell.cell_type = Preferences.CELL_TYPE_EMPTY
+        self.score += 1
+
+    # ==========================================
+    # 2.4 Enemy Movement Methods
+    # ==========================================
+
+    def add_enemy(self):
+        """Adds an enemy to the bottom-right corner of the board."""
+        br_cell = self.board[self.num_rows - 1][self.num_cols - 1]
         
-    def move_player_right(self) -> None:
-        """ Move the player one cell to the right if it is empty """
-        # TODO
-        pass # Remove this after you implement your function
+        # Check collision with player
+        if br_cell == self.player_cell:
+            self.game_over = True
+            return
 
-    def move_player_left(self) -> None:
-        """ Move the player one cell to the left if it is empty """
-        # TODO
-        pass # Remove this after you implement your function
+        # If bottom right has food, clear food tracking
+        if br_cell in self.food_cells:
+            self.food_cells.remove(br_cell)
 
-    def move_player_up(self) -> None:
-        """ Move the player one cell up if it is empty """
-        # TODO
-        pass # Remove this after you implement your function
+        br_cell.cell_type = Preferences.CELL_TYPE_ENEMY
+        if br_cell not in self.enemies:
+            self.enemies.append(br_cell)
 
-    def move_player_down(self) -> None:
-        """ Move the player one cell down if it is empty """
-        # TODO
-        pass # Remove this after you implement your function
+    def move_enemy_to_cell(self, idx, target_cell):
+        """Moves enemy at index idx to target_cell and checks collisions."""
+        if target_cell is None or idx >= len(self.enemies):
+            return
 
-    def move_player_to_cell(self, cell: Cell) -> None:
-        """ Move the player to the given cell """
+        current_cell = self.enemies[idx]
 
-        # If there is food in this cell, eat it
-        if cell.is_food():
-            self.eat_food(cell)
-            self.update_player_cell(cell)
-        # If there is an enemy in this cell, game over!
-        elif cell.is_enemy():
-            self.player.become_empty()
-            self.set_game_over()
-        # Otherwise, update the player location
-        else:
-            self.update_player_cell(cell)
+        # Check collision with player
+        if target_cell == self.player_cell:
+            self.game_over = True
+            return
 
-    def update_player_cell(self, new_cell: Cell) -> None:
-        """ Move the player to the new cell """
-    
-        # Empty the cell the player just moved away from
-        self.player.become_empty()
-        # Update the player to the new cell
-        self.player = new_cell
-        # Change the new cell to be the player type
-        self.player.become_player()
+        # Clear current enemy cell
+        current_cell.cell_type = Preferences.CELL_TYPE_EMPTY
+        
+        # Handle food overwrite
+        if target_cell in self.food_cells:
+            self.food_cells.remove(target_cell)
 
+        # Move enemy
+        target_cell.cell_type = Preferences.CELL_TYPE_ENEMY
+        self.enemies[idx] = target_cell
 
-    ########################
-    # Food Related Methods #
-    ########################
+    def move_enemy_left(self, idx):
+        """Moves enemy at index idx to the west neighbor cell."""
+        if idx < len(self.enemies):
+            target = self.get_west_neighbor(self.enemies[idx])
+            self.move_enemy_to_cell(idx, target)
 
-    def add_food(self) -> None:
-        """ Adds food to a random open spot on the board """
+    def move_enemy_right(self, idx):
+        """Moves enemy at index idx to the east neighbor cell."""
+        if idx < len(self.enemies):
+            target = self.get_east_neighbor(self.enemies[idx])
+            self.move_enemy_to_cell(idx, target)
 
-        # Find a row on the board
-        row = random.randrange(0, Preferences.NUM_ROWS)
-        # Find a col on the board
-        col = random.randrange(0, Preferences.NUM_COLS)
+    def move_enemy_up(self, idx):
+        """Moves enemy at index idx to the north neighbor cell."""
+        if idx < len(self.enemies):
+            target = self.get_north_neighbor(self.enemies[idx])
+            self.move_enemy_to_cell(idx, target)
 
-        # TODO
-        pass # Remove this after you implement your function
-
-    def eat_food(self, cell: Cell) -> None:
-        """ Behavior for when the player eats food """
-        # TODO
-        pass # Remove this after you implement your function
-
-
-    ##########################
-    # Enemy Movement Methods #
-    ##########################
-
-    def add_enemy(self) -> None:
-        """ Adds an enemy to the bottom right corner of the board """
-        # TODO
-        pass # Remove this after you implement your function
-
-    def move_enemy_to_cell(self, enemy_cell: Cell, 
-                           cell: Cell, idx: int) -> None:
-        """ Moves the enemy cell to a new location.
-            idx refpresents the index of that enemy in
-             the enemies list. """
-        # TODO
-        pass # Remove this after you implement your function
-
-    def move_enemy_left(self, idx: int) -> None:
-        """ Move the enemy at index idx left one cell """
-        # TODO
-        pass # Remove this after you implement your function
-
-    def move_enemy_right(self, idx: int) -> None:
-        """ Move the enemy at index idx right one cell """
-        # TODO
-        pass # Remove this after you implement your function
-
-    def move_enemy_up(self, idx: int) -> None:
-        """ Move the enemy at index idx up one cell """
-        # TODO
-        pass # Remove this after you implement your function
-
-    def move_enemy_down(self, idx: int) -> None:
-        """ Move the enemy at index idx down one cell """
-        # TODO
-        pass # Remove this after you implement your function
-
-
-
-if __name__ == "__main__":
-    gd = GameData()
-    # You can modify the line below for testing!
-    print(gd.get_west_neighbor(gd.player))
+    def move_enemy_down(self, idx):
+        """Moves enemy at index idx to the south neighbor cell."""
+        if idx < len(self.enemies):
+            target = self.get_south_neighbor(self.enemies[idx])
+            self.move_enemy_to_cell(idx, target)
